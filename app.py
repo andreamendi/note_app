dfb7f602d8b122a687ed3add24fef0526241fd64
AndreaMendi <andreamendi@icloud.com>
2018-11-07 22:17:49 -0600

    ya esta la conexion a la db

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-# from data import Notes
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+# from data import Notes
+
 
 
 app = Flask(__name__) 
@@ -14,17 +16,22 @@ app.config['MYSQL_PASSWORD'] = 'Mars24601@'
 app.config['MYSQL_DB'] = 'note_app'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
+
+
 #Init MySQL
 mysql  = MySQL(app)
 
+
+
 @app.route('/')
-def index():
+def index():  
     return render_template("home.html")
 
 @app.route('/notes')
 def my_notes():
     return render_template("notes.html")
 
+# El debajo se comento ya que era prueba.
 # @app.route('/note/<string:id>/')
 # def note(id):
 #     for note in notes:
@@ -32,6 +39,36 @@ def my_notes():
 #             data = note
 #             break
 #     return render_template("note.html", note = data)
+
+
+
+class NoteForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=45)])
+    description = TextAreaField('Description', [validators.Length(min=5)])
+
+@app.route('/add-note', methods=['GET','POST'])
+def add_note():
+    form = NoteForm(request.form)
+    
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        description = form.description.data
+
+        #Create a Cur -> o sea una conexion con la DB
+        cur = mysql.connection.cursor()
+
+        #Execute
+        cur.execute('INSERT INTO notes(title, description) VALUES (%s,%s)', (title,description))
+
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('add_note'))
+
+    return render_template('add_note.html', form=form)
+
+
+# El "if" debe de ir al final sino no va a correr
 
 if __name__ == '__main__':
     app.run(debug= True)
--- note_app.sql
+++ note_app.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE note_app;
+DROP DATABASE note_app;
+
 USE note_app;
 
 
 CREATE TABLE users(
-	id int NOT NULL,
+	id int NOT NULL auto_increment,
     name VARCHAR(45) NOT NULL,
     username VARCHAR(45) NOT NULL UNIQUE,
     password varchar(100) NOT NULL,
@@ -13,7 +15,7 @@ CREATE TABLE users(
 );
 
 CREATE TABLE notes(
-	id INT NOT NULL,
+	id INT NOT NULL auto_increment,
 	title VARCHAR (45),
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
